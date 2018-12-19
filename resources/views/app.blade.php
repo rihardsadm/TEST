2632ba9856b616e091bfb7efc1bbbfefd42c2cff
Rihards ADminis <rihards.adminis@applyit.lv>
2018-12-19 13:07:14 +0200

    Pievienoju index page

---
+++ resources/views/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Test</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" /> -->
+</head>
+<body>
+	<div class="container">
+					<h1 class="col-sm-12 text-center jumbotron">Heading</h1>
+		<div class="row jumbotron">
+			<div class="col-sm-4">
+			<h4>Heading</h4>
+			<p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo est similique, a fuga, eos, omnis facere totam hic aliquid debitis quam minima! Quibusdam ea harum nam unde maxime doloribus totam.</p>
+			</div>
+			<div class="col-sm-4">
+			<h4>Heading</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis quidem distinctio tempora sunt excepturi ducimus explicabo, adipisci autem voluptas alias, itaque amet iste eaque velit maxime. Laboriosam dolor aliquid, possimus!</p>
+			</div>
+			<div class="col-sm-4">
+			<h4>Heading</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis quos, laboriosam alias sunt. Laborum esse enim aspernatur alias unde, veritatis corrupti obcaecati eveniet dolore iure, adipisci, quae vero itaque maiores?</p>
+			</div>
+		</div>
+	<table class="table table-striped border">
+		<tr>
+			<th>Vards</th>
+			<th>Uzvards</th>
+			<th>Vecums</th>
+		</tr>
+		<tr>
+			<td>Janis</td>
+			<td>Kalnins</td>
+			<td>18</td>
+		</tr>
+		<tr>
+			<td>Maija</td>
+			<td>Liepa</td>
+			<td>12</td>
+		</tr>
+	</table>
+	</div>
+</body>
+</html>
